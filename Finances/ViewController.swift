73993de19ce58fdcd4a77d60b86e831d21770feb
Felipe Felicio <felipedfelicio@gmail.com>
2025-07-09 00:55:22 -0300

    Initial Commit

---
+++ Finances/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finances
+//
+//  Created by Felipe Felicio on 09/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
